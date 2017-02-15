13f5a1b3046dc8fde37b7ee98e3d2a5f61bcf9b9
Jeroen Jacobs <git@jeroenj.be>
2017-02-15 15:37:12 +0100

    Adds test which explicitly sets groups that works as expected

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -18,10 +18,20 @@ describe Project do
     it { is_expected.to validate_uniqueness_of(:name) }
   end
 
+  context "all is still good" do
+    it "does not fail if group is set explicitly" do
+      project = Project.new(name: "bar", groups: ["group"])
+      expect(project.changes).to eq({ "groups" => [[], ["group"]], "name" => [nil, "bar"] })
+      expect(project.save).to eq(true)
+      expect(project.groups).to eq(["group"])
+      expect(project.category).to eq("none")
+    end
+  end
+
   context "breakage" do
     it "no longer sets #groups to an empty array and category to none by default and fails because of an SQL ConstraintException" do
-      project = Project.new(name: "bar")
-      expect(project.changes).to eq({ "groups" => [[], []], "name" => [nil, "bar"] })
+      project = Project.new(name: "baz")
+      expect(project.changes).to eq({ "groups" => [[], []], "name" => [nil, "baz"] })
       expect(project.groups).to eq([])
       expect(project.category).to eq("none")
       expect { project.save }.to raise_error(ActiveRecord::StatementInvalid).with_message(/SQLite3::ConstraintException: NOT NULL constraint failed/)
